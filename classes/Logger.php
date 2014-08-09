4ee5551fdbc5d5e68aec11d9bce0a29318923498
Alexey Karunos <alkaruno@gmail.com>
2014-08-09 14:03:12 +0700

    Поддержка уровня логирования в логгере

--- classes/Logger.php
+++ classes/Logger.php
@@ -2,65 +2,88 @@
 
 class Logger
 {
-    const ALERT = 'alert';
-    const CRITICAL = 'critical';
-    const ERROR = 'error';
-    const WARNING = 'warning';
-    const NOTICE = 'notice';
-    const INFO = 'info';
-    const DEBUG = 'debug';
+    const EMERGENCY = 'emergency';
+    const ALERT     = 'alert';
+    const CRITICAL  = 'critical';
+    const ERROR     = 'error';
+    const WARNING   = 'warning';
+    const NOTICE    = 'notice';
+    const INFO      = 'info';
+    const DEBUG     = 'debug';
 
-    public static function alert($message)
+    public function emergency($message, array $context = null)
     {
-        self::log($message, self::ALERT);
+        self::log(self::EMERGENCY, $message, $context);
     }
 
-    public static function critical($message)
+    public static function alert($message, array $context = null)
     {
-        self::log($message, self::CRITICAL);
+        self::log(self::ALERT, $message, $context);
     }
 
-    public static function error($message)
+    public static function critical($message, array $context = null)
     {
-        self::log($message, self::ERROR);
+        self::log(self::CRITICAL, $message, $context);
     }
 
-    public static function warning($message)
+    public static function error($message, array $context = null)
     {
-        self::log($message, self::WARNING);
+        self::log(self::ERROR, $message, $context);
     }
 
-    public static function notice($message)
+    public static function warning($message, array $context = null)
     {
-        self::log($message, self::NOTICE);
+        self::log(self::WARNING, $message, $context);
     }
 
-    public static function info($message)
+    public static function notice($message, array $context = null)
     {
-        self::log($message, self::INFO);
+        self::log(self::NOTICE, $message, $context);
     }
 
-    public static function debug($message)
+    public static function info($message, array $context = null)
     {
-        self::log($message, self::DEBUG);
+        self::log(self::INFO, $message, $context);
     }
 
-    public static function log($message, $level = self::INFO)
+    public static function debug($message, array $context = null)
     {
-        if (!in_array($level, array(self::ALERT, self::CRITICAL, self::ERROR, self::ERROR, self::WARNING, self::NOTICE, self::INFO, self::DEBUG))) {
-            throw new Exception('Illegal logger level: ' . $level, 500);
+        self::log(self::DEBUG, $message, $context);
+    }
+
+    public static function log($level, $message, array $context = null)
+    {
+        $loggerLevel = isset(App::$config['logging']['level']) ? App::$config['logging']['level'] : self::WARNING;
+
+        $class = new ReflectionClass('Logger');
+        foreach ($class->getConstants() as $const => $value) {
+            if ($value == $level) {
+                self::internalLog($level, $message, $context);
+                return;
+            }
+            if ($value == $loggerLevel) {
+                return;
+            }
         }
+    }
 
+    private static function internalLog($level, $message, $context)
+    {
         if (is_array($message)) {
             $message = print_r($message, true);
         }
 
-        $filename = sprintf('../app/logs/%s.log', date('Y-m-d'));
+        $message .= is_array($context) ? ': ' . print_r($context, true) : PHP_EOL;
 
+        $filename = sprintf('../app/logs/%s.log', date('Y-m-d'));
         if (!is_dir(dirname($filename))) {
             mkdir(dirname($filename), 0777, true);
         }
 
-        file_put_contents($filename, sprintf('%s %s %s%s', date('Y.m.d H:i:s'), strtoupper($level), $message, PHP_EOL), FILE_APPEND);
+        file_put_contents(
+            $filename,
+            sprintf('%s %s %s', date('Y.m.d H:i:s'), strtoupper($level), $message),
+            FILE_APPEND
+        );
     }
 }
