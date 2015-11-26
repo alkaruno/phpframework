9b3853e44d83f243ba072dc8616d0d1f3f8684ef
Alexey Karunos <alkaruno@gmail.com>
2015-11-26 20:27:11 +0600

    Полировка кода

--- src/RouterController.php
+++ src/RouterController.php
@@ -2,21 +2,19 @@
 
 namespace Xplosio\PhpFramework;
 
-use Exception;
-
 abstract class RouterController extends Controller
 {
     public function handle()
     {
         $routes = $this->getRoutes();
-        if ($routes != null) {
+        if ($routes !== null) {
             list($method, $params) = App::route($this->request->getUri(), $routes);
             if (is_callable(array($this, $method))) {
                 return call_user_func_array(array($this, $method), $params);
             }
         }
 
-        throw new Exception('Handler not found', 404);
+        throw new \InvalidArgumentException('Handler not found', 404);
     }
 
     abstract protected function getRoutes();
